31055d7142753ca6eeb07db740346490ad7e4cc6
Gabriel Carvalho <79951027+Carvalhocodes@users.noreply.github.com>
2025-06-25 10:04:53 -0300

    Create matrizes.js
    feat: adiciona exemplos de arrays e matrizes.

---
+++ matrizes.js
@@ -0,0 +1,16 @@
+// Arrays simples em listas de compras!
+let frutas = ["maças", "banana", "mamão", "melancia"]
+console.log(frutas [0]);
+
+// Arrays simples em contagem de números!
+let numeros = [1, 2, 3]
+numeros.push (4)
+console.log (numeros);
+
+// Matriz simples para selecionar a prateleira da despensa!
+let despensa =  [
+    ["arroz", "feijão", "macarrão"],
+    ["farinha", "molho de tomate", "azeite"],
+    ["bolacha", "salgadinho", "doces"]
+]
+console.log (despensa[0][1]);
